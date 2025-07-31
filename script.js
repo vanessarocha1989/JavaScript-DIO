49690f7d0e6c1ad1da59c4cc0047deb67d6da7ba
Vanessa Rocha <142359831+vanessarocha1989@users.noreply.github.com>
2025-07-31 17:29:04 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let idade = 35
+console.log("valor da minha variavel " + idade)
+
+idade = 35 + 7
+console.log("operação de adição " + idade)
+
+let primeiroNumero = 1023
+let segundoNumero = 23
+console.log(primeiroNumero - segundoNumero)
+
+let multiplicador = 4
